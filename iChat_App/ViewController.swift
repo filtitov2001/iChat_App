b1f7ac9b204b0c2c7bc2acf5b7f211d3e3fb67ed
Felix Titov <titow.feliks@yandex.ru>
2022-07-09 11:59:46 +0300

    Initial Commit

---
+++ iChat_App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iChat_App
+//
+//  Created by Felix Titov on 7/9/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
