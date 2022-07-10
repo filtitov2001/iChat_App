934fb995d1839dba0aadc7df9c01e2a739cd2e77
Felix Titov <titow.feliks@yandex.ru>
2022-07-10 21:58:24 +0300

    Created Cell GradientView

--- iChat_App/Views/Cells/ActiveChatCell.swift
+++ iChat_App/Views/Cells/ActiveChatCell.swift
@@ -20,7 +20,7 @@ class ActiveChatCell: UICollectionViewCell, SelfConfigureCell {
     let friendImageView = UIImageView()
     let friendName = UILabel(text: "User name", font: .laoSangamMN20())
     let lastMessage = UILabel(text: "Fuck you!", font: .laoSangamMN18())
-    let gradientView = UIView()
+    let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: #colorLiteral(red: 0.7882352941, green: 0.631372549, blue: 0.9411764706, alpha: 1), endColor: #colorLiteral(red: 0.4784313725, green: 0.6980392157, blue: 0.9215686275, alpha: 1))
     
     
     override init(frame: CGRect) {
